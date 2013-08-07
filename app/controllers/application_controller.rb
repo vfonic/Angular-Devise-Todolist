4e71ef19915f4b70e5ab88b7a053b6be31ac654c
Viktor Fonic <viktor.fonic@gmail.com>
2013-08-07 15:30:03 +0200

    Fix: instruct devise to redirect to root after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
