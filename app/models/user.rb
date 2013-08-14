fa3e4d4179c44efbc801113b7265faccdad7a174
Viktor Fonic <viktor.fonic@gmail.com>
2013-08-14 12:31:36 +0200

    Add token_authenticatable for REST API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable
+  before_save :ensure_authentication_token
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
   has_many :tasks
+
+  def as_json(options = {})
+    {
+        id: id,
+        name: name,
+        email: email,
+        created_at: created_at,
+        updated_at: updated_at,
+        auth_token: authentication_token
+    }
+  end
 end
